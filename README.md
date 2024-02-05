81216f5e321860e38d7951cb98f4bb01df539f3c
Sameema Nasrin <sameemanasrin23@gmail.com>
2024-02-05 21:21:53 +0530

    updated name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SameemaPortfolio
+# aminaPortfolio
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.9.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sameemaPortfolio'`, () => {
+  it(`should have as title 'aminaPortfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sameemaPortfolio');
+    expect(app.title).toEqual('aminaPortfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sameemaPortfolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('aminaPortfolio app is running!');
   });
 });
